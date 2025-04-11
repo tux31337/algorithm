7d3f09639c61c88df42ae12f166f36eb6e3fc684
tux31337 <ljm920604@gmail.com>
2025-04-11 10:12:28 +0900

    Feat. 백준 투포인터 기토문제

---
+++ src/pratice/Baekjoon2003.java
@@ -0,0 +1,59 @@
+package pratice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon2003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 수열 개 수
+        int N = scanner.nextInt();
+
+        // 찾아야 할 합의 값
+        int M = scanner.nextInt();
+
+        int result = 0;
+
+        System.out.println("M = " + M);
+
+        // 수열을 담을 배열과 수열을 입력
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        int startIndex = 0;
+        int endIndex =  0;
+
+
+
+        while (endIndex != N) {
+            int sum = calculateSum(startIndex, endIndex, arr);
+
+            if (sum < M) {
+                endIndex++;
+            } else if (sum == M) {
+                result++;
+                endIndex++;
+            } else {
+                startIndex++;
+            }
+        }
+
+        System.out.println("result = " + result);
+    }
+
+    static int calculateSum(int startIndex, int endIndex, int[] arr) {
+        int sum = 0;
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            sum+=arr[i];
+        }
+
+        return sum;
+    }
+}
---
+++ src/pratice/Baekjoon2018.java
@@ -0,0 +1,34 @@
+package pratice;
+
+import java.util.Scanner;
+
+public class Baekjoon2018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 자연수 N
+        int N = scanner.nextInt();
+        int count = 1;
+        int startIndex = 1;
+        int endIndex = 1;
+        int sum = 1;
+
+        while (endIndex != N) {
+            if(sum == N) {
+                System.out.println("startIndex = " + startIndex);
+                System.out.println("endIndex = " + endIndex);
+                count++;
+                endIndex++;
+                sum+=endIndex;
+            } else if(sum > N) {
+                sum-=startIndex;
+                startIndex++;
+            } else {
+                endIndex++;
+                sum+=endIndex;
+            }
+        }
+
+        System.out.println("count = " + count);
+    }
+}
